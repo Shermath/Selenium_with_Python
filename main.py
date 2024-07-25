5604aaef04167e23f940ee017ae37b9e29725290
sherin.mathew@gmail.com <sherin.mathew@gmail.com>
2024-07-25 23:37:38 +1000

    Simply Open a search a string

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.keys import Keys
 import time
 
 service=Service(executable_path="chromedriver.exe")
@@ -7,11 +9,9 @@ driver=webdriver.Chrome(service=service)
 
 driver.get("https://google.com")
 
+input_element = driver.find_element(By.CLASS_NAME,"truncate")
+input_element.send_keys("tech with tim")
+
 time.sleep(10)
 
 driver.quit()
-
-#browser = webdriver.Chrome(
- #   executable_path="chromedriver.exe")
-
-#browser.get('https://www.google.com')
